618cf9dbb527ecfe6e3e7ecb87c0bd5ee67872c3
RajatDatta5315 <rajatdatta90000@gmail.com>
2026-01-17 17:24:51 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,12 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
+
 export default clerkMiddleware();
-export const config = { matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'] };
+
+export const config = {
+  // Isse Clerk aur Vercel ka conflict khatam ho jayega
+  runtime: 'nodejs', 
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
