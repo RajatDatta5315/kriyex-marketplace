f40c37a226b8a35a2c6f7b3d135409e13d925af1
RajatDatta5315 <rajatdatta90000@gmail.com>
2026-01-22 01:29:18 +0530

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-api-js'
+
+const supabaseUrl = 'TERA_SUPABASE_URL'
+const supabaseKey = 'TERA_SUPABASE_ANON_KEY'
+export const supabase = createClient(supabaseUrl, supabaseKey)
